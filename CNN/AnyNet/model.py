444d8b6939a14533c1f6a3370aa4d41def76e6a7
Hubert <250367@edu.p.lodz.pl>
2026-08-06 16:17:27 +0200

    ToDo done + tyided up code

--- CNN/AnyNet/model.py
+++ CNN/AnyNet/model.py
@@ -1,29 +1,48 @@
-import typing
-from typing import Any
-
 import torch
 from torch import nn
-from torch.distributions import one_hot_categorical
 from torch.nn import functional as F
 
 class ResNextBLK(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, width_of_group: int, use_1x1_conv : bool=False, bottleneck_multiplier: float= 1.0, strides: int = 1) -> None:
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            width_of_group: int,
+            use_1x1_conv : bool=False,
+            bottleneck_multiplier: float= 1.0,
+            strides: int = 1
+    ) -> None:
+
         super().__init__()
         bottleneck_channels = int(round(out_channels * bottleneck_multiplier))
 
         # if not use_1x1_conv: #1x1 is only used for the first block in a stage different stages have different
         #     # in_channels but the rest of the block in a stage follow the same in_channels as the out_channel
-        #     in_channels = bottleneck_channels
-        self.conv1 = nn.Conv2d(in_channels, bottleneck_channels, kernel_size=1, stride=1)
-        self.conv2 = nn.Conv2d(bottleneck_channels, bottleneck_channels, kernel_size=3,
-                                stride=strides, padding=1, groups=bottleneck_channels//width_of_group)
-        self.conv3 = nn.Conv2d(bottleneck_channels, out_channels, kernel_size=1, stride=1)
+        self.conv1 = nn.Conv2d(
+            in_channels, bottleneck_channels, kernel_size=1, stride=1
+        )
+        self.conv2 = nn.Conv2d(
+            bottleneck_channels,
+            bottleneck_channels,
+            kernel_size=3,
+            stride=strides,
+            padding=1,
+            groups=bottleneck_channels//width_of_group
+        )
+        self.conv3 = nn.Conv2d(
+            bottleneck_channels,
+            out_channels,
+            kernel_size=1,
+            stride=1
+        )
 
         self.bn1 = nn.BatchNorm2d(bottleneck_channels)
         self.bn2 = nn.BatchNorm2d(bottleneck_channels)
         self.bn3 = nn.BatchNorm2d(out_channels)
         if use_1x1_conv:
-            self.conv4 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=strides)
+            self.conv4 = nn.Conv2d(
+                in_channels, out_channels, kernel_size=1, stride=strides
+            )
 
             self.bn4 = nn.BatchNorm2d(out_channels)
         else:
@@ -40,28 +59,71 @@ class ResNextBLK(nn.Module):
         return F.relu(Y + X)
 
 class AnyNet(nn.Module):
-    def __init__(self, arch, input_channels, stem_channels, num_classes=10, lr=0.1): #TODO I ahve no idea wh I coppied the lr here but it was there in a notebook I am learning from.
+    def __init__(
+            self,
+            arch,
+            input_channels,
+            stem_channels,
+            num_classes=10,
+            ):
         super().__init__()
-        self.net = nn.Sequential(self.stem(input_channels, stem_channels), self.body(arch))
-        self.net.add_module("head", nn.Sequential(
-            nn.AdaptiveMaxPool2d((1,1)), nn.Flatten(),
-            nn.Linear(arch[-1][2],num_classes)
-        ))
+        self.net = nn.Sequential(
+            self.stem(input_channels, stem_channels), self.body(arch)
+        )
+        self.net.add_module(
+            "head",
+            nn.Sequential(
+                nn.AdaptiveMaxPool2d((1,1)),
+                nn.Flatten(),
+                nn.Linear(arch[-1][2],num_classes)
+            )
+        )
 
     def forward(self, X):
         return self.net(X)
 
-    def stem(self,in_channels: int, out_channels: int) -> nn.Sequential:
-        return nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1),
-                             nn.BatchNorm2d(out_channels), nn.ReLU())
+    def stem(self, in_channels: int, out_channels: int) -> nn.Sequential:
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size=3,
+                stride=2,
+                padding=1
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
 
-    def stage(self, depth, in_channels, out_channels, width_of_groups, bot_mul: float=1.0):
+    def stage(
+            self,
+            depth,
+            in_channels,
+            out_channels,
+            width_of_groups,
+            bot_mul: float=1.0
+    ):
         blk = []
         for i in range(depth):
             if i ==0:
-                blk.append(ResNextBLK(in_channels, out_channels, width_of_groups, use_1x1_conv=True, bottleneck_multiplier=bot_mul))
+                blk.append(
+                    ResNextBLK(
+                        in_channels,
+                        out_channels,
+                        width_of_groups,
+                        use_1x1_conv=True,
+                        bottleneck_multiplier=bot_mul
+                    )
+                )
             else:
-                blk.append(ResNextBLK(out_channels, out_channels, width_of_groups, bottleneck_multiplier=bot_mul)) #except for the first block in a stage
+                blk.append(
+                    ResNextBLK(
+                        out_channels,
+                        out_channels,
+                        width_of_groups,
+                        bottleneck_multiplier=bot_mul
+                    )
+                ) #except for the first block in a stage
                 #all of the other blocks have the same number of channels
         return nn.Sequential(*blk)
 
@@ -84,10 +146,20 @@ def initialize_xavier(module: nn.Module):
 
 
 class RegNet(AnyNet):
-    def __init__(self, depths: tuple, in_channels: tuple, out_channels: tuple, groups_width, bottleneck_multiplier,
-                 input_channels, stem_channels, num_classes=10, lr=0.1):
+    def __init__(
+            self,
+            depths: tuple,
+            in_channels: tuple,
+            out_channels: tuple,
+            groups_width,
+            bottleneck_multiplier,
+            input_channels,
+            stem_channels,
+            num_classes=10
+    ):
         arch = []
         for depth, in_channels, out_channels in zip(depths, in_channels, out_channels):
-            arch.append((depth, in_channels, out_channels, groups_width, bottleneck_multiplier))
-
-        super().__init__(arch, input_channels, stem_channels, num_classes, lr)
+            arch.append(
+                (depth, in_channels, out_channels, groups_width, bottleneck_multiplier)
+            )
+        super().__init__(arch, input_channels, stem_channels, num_classes)

b39b8b0fc40e3c84fb1234712a8f5d8a4d3cfbfe
Hubert <250367@edu.p.lodz.pl>
2026-08-09 20:37:03 +0200

    Normlaized the input (OMG so LATE) added data augmentation.

--- CNN/AnyNet/data.py
+++ CNN/AnyNet/data.py
@@ -1,6 +1,6 @@
 import torch
 import torchvision
-from torchvision import transforms
+from torchvision.transforms import v2
 
 class DeviceDataLoader:
     def __init__(self, dataloader: torch.utils.data.DataLoader, device: torch.device):
@@ -20,10 +20,35 @@ class FashionMNIST:
         self.batch_size = batch_size
         self.device = device
         self.num_of_workers = num_of_workers
-        transform = transforms.Compose([transforms.Resize(input_shape), transforms.ToTensor()])
+        train_transform = v2.Compose([
+            # Keep as ToImage() + ToDtype(uint8) to preserve the ultra-fast uint8 pipeline
+            v2.ToImage(),
+            v2.ToDtype(torch.uint8, scale=True),
 
-        self.training_data = torchvision.datasets.FashionMNIST(root=dir, transform=transform, train=True, download=True)
-        self.validation_data = torchvision.datasets.FashionMNIST(root=dir, transform=transform, train=False, download=True)
+            # Efficient uint8 spatial operations
+            v2.Resize(size=input_shape, interpolation=v2.InterpolationMode.BILINEAR, antialias=True),
+            v2.RandomHorizontalFlip(p=0.5),
+            # v2.RandomRotation(degrees=(-12,12)),
+            # v2.RandomAffine(degrees=(0,0), translate=(0.08, 0.08), scale=(0.92, 1.08)),
+
+            # Delayed casting to float32 happens at the very end
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.2860], std=[0.3530])
+        ])
+
+        val_transform = v2.Compose([
+            v2.ToImage(),
+            v2.ToDtype(torch.uint8, scale=True),
+            v2.Resize(size=input_shape, interpolation=v2.InterpolationMode.BILINEAR, antialias=True),
+
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.2860], std=[0.3530])
+        ])
+
+        self.training_data = torchvision.datasets.FashionMNIST(root=dir, transform=train_transform, train=True,
+                                                               download=True)
+        self.validation_data = torchvision.datasets.FashionMNIST(root=dir, transform=val_transform, train=False,
+                                                                 download=True)
 
     def get_training_data(self):
         use_pin = self.device.type in ["cuda", "mps"]
